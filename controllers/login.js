03bc0a05437a04275b24b3006f3d4ca88f24b42a
alecnissen <alecnissen@yahoo.com>
2024-10-02 23:16:08 -0400

    working on passport authentication method, saving work

--- controllers/login.js
+++ controllers/login.js
@@ -1,8 +1,61 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
+const { body, validationResult } = require("express-validator");
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
 
 const { PrismaClient } = require('@prisma/client');
+
 const prisma = new PrismaClient();
+
+
+passport.use( 
+  new LocalStrategy( 
+    { usernameField: 'username', passwordField: 'password', passReqToCallback: true },
+  
+    async (req, email, password, done) => { 
+      try { 
+        const user = await prisma.users.findUnique({ 
+          where: { 
+            username: req.body.username
+          }
+        })
+        if (!user) { 
+          console.log('Incorrect Username');
+          return done(null, false, { message: 'Incorrect Username'})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) { 
+          console.log('Incorrect Password');
+          return done(null, false, { message: 'Incorrect Password' });
+        }
+
+        console.log('MATCH');
+        return done(null, user);
+
+      
+    } catch (err) { 
+      return done(err);
+    }
+  }
+  )
+)
+
+passport.serializeUser((user, done) => { 
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => { 
+  try { 
+    const user = await prisma.users.findUnique({ 
+      where: { id: id }
+    })
+  } catch (err) { 
+    done(err);
+  }
+})
+
 
 
 exports.log_in_get = (req, res, next) => {
@@ -10,7 +63,7 @@ exports.log_in_get = (req, res, next) => {
   };
 
 
-exports.log_in_post = (req, res, next) => { 
+  exports.log_in_post = (req, res, next) => { 
     try {
         console.log("Request body:", req.body);
         res.redirect('/');
