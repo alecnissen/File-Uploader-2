7234f4ec3f6c034fce2715d85054879abade49ad
alecnissen <alecnissen@yahoo.com>
2025-01-05 21:22:15 -0500

    added readme file, will begin to fill out file

--- index.js
+++ index.js
@@ -5,27 +5,34 @@ const prisma = new PrismaClient();
 async function main() {
   // ... you will write your Prisma Client queries here
 
-  const allUsers = await prisma.users.findMany();
-  console.log(allUsers);
+  // await prisma.users.deleteMany({});
+  // console.log('All users deleted');
 
-  const allFolders = await prisma.folder.findMany({
-    include: {
-      files: true
-    }
-  });
-  console.log(allFolders);
+  // await prisma.folder.deleteMany({});
+  // console.log('All folders deleted');
 
-  allFolders.forEach((folder) => {
-    console.log(`Folder: ${folder.name}, Created At: ${folder.createdAt}`);
+  // const allUsers = await prisma.users.findMany();
+  // console.log(allUsers);
 
-    if (folder.files.length > 0) {
-      folder.files.forEach((file) => {
-        console.log(`  File Name: ${file.fileName}, Created At: ${file.createdAt}`);
-      });
-    } else {
-      console.log('  No files found for this folder');
-    }
-  });
+  // const allFolders = await prisma.folder.findMany({
+  //   include: {
+  //     files: true
+  //   }
+  // });
+  // console.log(allFolders);
+
+
+  // allFolders.forEach((folder) => {
+  //   console.log(`Folder: ${folder.name}, Created At: ${folder.createdAt}`);
+
+  //   if (folder.files.length > 0) {
+  //     folder.files.forEach((file) => {
+  //       console.log(`  File Name: ${file.fileName}, Created At: ${file.createdAt}`);
+  //     });
+  //   } else {
+  //     console.log('  No files found for this folder');
+  //   }
+  // });
 }
 
 main()
